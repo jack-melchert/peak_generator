23ec969d9dd52e55cbfb08d9036dd50ff4af0470
Jackson Melchert <melchert@stanford.edu>
2020-04-02 20:54:47 -0700

    Trying more things to get travis to work

--- scripts/gen_rtl.py
+++ scripts/gen_rtl.py
@@ -1,11 +1,14 @@
 from peak_gen.sim import arch_closure
 from peak_gen.arch import read_arch, graph_arch
 import magma as m
-import sys
+import sys, os
 
 arch = read_arch(str(sys.argv[1]))
 PE_fc = arch_closure(arch)
 PE = PE_fc(m.get_family())
+
+if not os.path.exists('scripts/build'):
+    os.makedirs('scripts/build')
 m.compile(f"scripts/build/PE", PE, output="coreir-verilog")
 
 
--- tests/test_gen_rtl.py
+++ tests/test_gen_rtl.py
@@ -6,9 +6,14 @@ import pdb
 import pytest
 import glob, os
 
+
 @pytest.mark.parametrize("arch_file", glob.glob('examples/**/*.json', recursive=True))
 def test_gen_rtl(arch_file):
     arch = read_arch(arch_file)
     PE_fc = arch_closure(arch)
     PE = PE_fc(m.get_family())
+
+    if not os.path.exists('tests/build'):
+        os.makedirs('tests/build')
+
     m.compile(f"tests/build/PE", PE, output="coreir-verilog")
