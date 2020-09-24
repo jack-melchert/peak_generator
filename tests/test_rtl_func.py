f93e4e4bcf9bc227b11dd7fb4055364843163f61
Jackson Melchert <melchert@stanford.edu>
2020-09-24 09:02:28 -0700

    Fixed rtl test i think

--- tests/test_rtl_func.py
+++ tests/test_rtl_func.py
@@ -15,89 +15,7 @@ import random
 import pytest
 from rtl_utils import rtl_tester
 
-def test_add():
-    arch = read_arch("examples/misc_tests/test_alu.json")
-    PE_fc = pe_arch_closure(arch)
-    inst_gen = asm_arch_closure(arch)(family.PyFamily())
-    PE_bv = PE_fc.Py()
-    
-    inputs = [BitVector.random(16),BitVector.random(16)]
 
-    res_comp = inputs[0] + inputs[1]
-
-    inst = inst_gen()
-
-    res_pe_bv = PE_bv(inst, inputs)
-
-    assert res_comp == res_pe_bv[0].value
-    rtl_tester(arch, inst, inputs, res_comp)
-
-
-# def test_mul():
-#     arch = read_arch("examples/misc_tests/test_mul.json")
-#     PE_fc = pe_arch_closure(arch)
-#     inst_gen = asm_arch_closure(arch)(family.PyFamily())
-#     PE_bv = PE_fc.Py()
-    
-#     inputs = [BitVector.random(16),BitVector.random(16)]
-
-#     res_comp = inputs[0] * inputs[1]
-#     breakpoint()
-
-#     inst = inst_gen()
-
-#     res_pe_bv = PE_bv(inst, inputs)
-
-#     assert res_comp == res_pe_bv[0].value
-
-#     rtl_tester(arch, inst, inputs, res_comp)
-
-# def test_alu():
-#     arch = read_arch("examples/misc_tests/test_alu.json")
-#     PE_fc = pe_arch_closure(arch)
-#     inst_gen = asm_arch_closure(arch)(family.PyFamily())
-#     PE_bv = PE_fc.Py()
-
-#     inputs = [BitVector.random(16),BitVector.random(16)]
-
-#     res_comp = inputs[0] + inputs[1]
-#     res_pe_bv = PE_bv(inst_gen(alu=[ALU_t.Add]), inputs)
-#     assert res_comp == res_pe_bv[0].value
-
-#     res_comp = inputs[0] - inputs[1]
-#     res_pe_bv = PE_bv(inst_gen(alu=[ALU_t.Sub]), inputs)
-#     assert res_comp == res_pe_bv[0].value
-
-
-# def test_multiple_outs():
-#     arch = read_arch("examples/misc_tests/test_multiple_outs.json")
-#     PE_fc = pe_arch_closure(arch)
-#     inst_gen = asm_arch_closure(arch)(family.PyFamily())
-#     PE_bv = PE_fc.Py()
-
-#     inputs = [BitVector.random(8),BitVector.random(8)]
-
-#     res_comp = inputs[0] + inputs[1]
-#     res_pe_bv = PE_bv(inst_gen(alu=[ALU_t.Add]), inputs)
-#     assert res_comp == res_pe_bv[0].value
-
-#     res_comp = inputs[0] * inputs[1]
-#     assert res_comp == res_pe_bv[1].value
-
-# def test_fma():
-#     arch = read_arch("examples/misc_tests/fma_pipelined.json")
-#     PE_fc = pe_arch_closure(arch)
-#     inst_gen = asm_arch_closure(arch)(family.PyFamily())
-#     PE_bv = PE_fc.Py()
-
-#     inputs = [BitVector.random(8),BitVector.random(8),BitVector.random(8)]
-
-#     res_comp = inputs[0] * inputs[1] + inputs[2]
-#     res_pe_bv = PE_bv(inst_gen(), inputs)
-#     assert inputs[2] == res_pe_bv[0].value
-
-#     res_pe_bv = PE_bv(inst_gen(), inputs)
-#     assert res_comp == res_pe_bv[0].value
 
 def test_mux():
 
@@ -116,12 +34,4 @@ def test_mux():
 
     assert res_comp == res_pe_bv[0].value
     rtl_tester(arch, inst, inputs, res_comp)
-
-    # print("Passed 1")
-
-    # inst = inst_gen(mux_out=[BitVector[1](1)])
-    # res_comp = inputs[0] * inputs[1]
-    # res_pe_bv = PE_bv(inst, inputs)
-    # assert res_comp == res_pe_bv[0].value
-    # rtl_tester(arch, inst, inputs, res_comp)
 
